c6e6c7aca0e9883ed4980da5224b49e28e58123b
amj133 <amj@vt.edu>
2018-06-01 10:46:21 -0600

    create comment_spec

---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Comment do
+  describe "validations" do
+    it { should validate_presence_of(:body) }
+  end
+
+  describe "relationships" do
+    it {should belong_to(:user) }
+    it {should belong_to(:article) }
+  end
+
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,8 @@ describe User do
     it { should validate_uniqueness_of :username }
     it { should validate_presence_of :password }
   end
+
+  describe "relationships" do
+    it { should have_many(:articles) }
+  end
 end
