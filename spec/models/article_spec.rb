048276689e166b363adfd421b4af845f823f721a
amj133 <amj@vt.edu>
2018-06-01 10:22:47 -0600

    create article model - spec passing

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe Article do
+  describe "validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:body) }
+  end
+
+  describe "relationships" do
+    it { should belong_to(:user) }
+  end
+end
